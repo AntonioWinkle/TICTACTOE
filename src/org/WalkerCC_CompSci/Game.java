529aeeb8813a2dcafbb5af9e0c24477b2184d56a
AntonioWinkle <winklant000@warren.k12.in.us>
2022-03-21 11:41:32 -0400

    done

--- src/org/WalkerCC_CompSci/Game.java
+++ src/org/WalkerCC_CompSci/Game.java
@@ -64,12 +64,63 @@ public class Game {
     }
 
     private boolean CheckForWin() {
-        //Implement this method.
+
+        if(this.gameBoard.spaces[0] == this.gameBoard.spaces[1] && this.gameBoard.spaces[1] == this.gameBoard.spaces[2]){
+            if (this.gameBoard.spaces[0] != Space.EMPTY){
+                return true;
+            }
+        } else if (this.gameBoard.spaces[3] == this.gameBoard.spaces[4] && this.gameBoard.spaces[4] == this.gameBoard.spaces[5]) {
+            if (this.gameBoard.spaces[3] != Space.EMPTY) {
+                return true;
+            }
+        } else if (this.gameBoard.spaces[6] == this.gameBoard.spaces[7] && this.gameBoard.spaces[7] == this.gameBoard.spaces[8]) {
+            if (this.gameBoard.spaces[6] != Space.EMPTY) {
+                return true;
+            }
+        } else if (this.gameBoard.spaces[0] == this.gameBoard.spaces[3] && this.gameBoard.spaces[3] == this.gameBoard.spaces[6]) {
+            if (this.gameBoard.spaces[0] != Space.EMPTY) {
+                return true;
+            }
+        } else if (this.gameBoard.spaces[1] == this.gameBoard.spaces[4] && this.gameBoard.spaces[4] == this.gameBoard.spaces[7]) {
+            if (this.gameBoard.spaces[1] != Space.EMPTY) {
+                return true;
+            }
+        } else if (this.gameBoard.spaces[2] == this.gameBoard.spaces[5] && this.gameBoard.spaces[5] == this.gameBoard.spaces[8]) {
+            if (this.gameBoard.spaces[2] != Space.EMPTY) {
+                return true;
+            }
+        } else if (this.gameBoard.spaces[0] == this.gameBoard.spaces[4] && this.gameBoard.spaces[4] == this.gameBoard.spaces[8]) {
+            if (this.gameBoard.spaces[0] != Space.EMPTY) {
+                return true;
+            }
+        } else if (this.gameBoard.spaces[2] == this.gameBoard.spaces[4] && this.gameBoard.spaces[4] == this.gameBoard.spaces[6]) {
+            if (this.gameBoard.spaces[2] != Space.EMPTY) {
+                return true;
+            }
+        }
+
         return false;
+
     }
 
     private boolean IsFilled(){
-        //Implement this method.
+
+        for(int i = 0; i < gameBoard.spaces.length; i++){
+
+            if((this.gameBoard.spaces[0] != Space.EMPTY) &&
+                    (this.gameBoard.spaces[1] != Space.EMPTY) &&
+                    (this.gameBoard.spaces[2] != Space.EMPTY) &&
+                    (this.gameBoard.spaces[3] != Space.EMPTY) &&
+                    (this.gameBoard.spaces[4] != Space.EMPTY) &&
+                    (this.gameBoard.spaces[5] != Space.EMPTY) &&
+                    (this.gameBoard.spaces[6] != Space.EMPTY) &&
+                    (this.gameBoard.spaces[7] != Space.EMPTY) &&
+                    (this.gameBoard.spaces[8] != Space.EMPTY)){
+                return true;
+            }
+
+        }
+
         return false;
     }
 
